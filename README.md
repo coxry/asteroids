2842d438c5d64668bfa8154c8d434dc920d18f71
Raymond Cox <rj.cox101@gmail.com>
2014-09-07 21:27:44 -0400

    Add grunt build tasks

---
+++ README.md
@@ -0,0 +1,13 @@
+Asteroids
+=========
+An asteroids clone
+
+Building
+--------
+1. `npm install && bower install`
+2. `grunt`
+3. The files are compiled to `dist`
+
+Developing
+----------
+`grunt watch`
---
+++ gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+  watch: {
+    scripts: {
+      files: ['app/*.html', 'app/*.coffee'],
+      tasks: ['default'],
+      options: {
+        spawn: false,
+      }
+    }
+  },
+
+    coffeelint: {
+      options: {
+        configFile: 'coffeelint.json'
+      },
+      app: ['app/*.coffee']
+    },
+    coffee: {
+      compile: {
+        files: {
+          'tmp/asteroids.js': 'app/*.coffee'
+        }
+      }
+    },
+    concat: {
+      dist: {
+        src: ['vendor/rAF.js',
+              'bower_components/es5-shim/es5-shim.js',
+              'bower_components/es5-shim/es5-sham.js',
+              'bower_components/es6-promise/promise.js',
+              'bower_components/jquery/dist/jquery.js',
+              'bower_comonents/bootstrap/dist/js/bootstrap.js',
+              'tmp/asteroids.js'],
+        dest: 'tmp/asteroids.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'tmp/asteroids.js',
+        dest: 'dist/asteroids.min.js'
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          { src: ['bower_components/bootstrap/dist/css/bootstrap.min.css'], dest: 'dist/style.css' },
+          { src: ['**/*'], cwd: 'app/assets/images', expand: true, flatten: true, dest: 'dist/images/' },
+          { src: ['app/index.html'], dest: 'dist/index.html' }
+        ]
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-coffeelint');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  // grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['coffeelint', 'coffee', 'concat', 'uglify', 'copy']);
+};
